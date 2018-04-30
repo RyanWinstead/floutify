428fb373887a41aeaecb0574463ea38c9597f435
RyanWinstead <ryanwinstead33@yahoo.com>
2018-04-30 00:29:38 -0700

    put image processing in imagePros function

--- ContourCounting.py
+++ ContourCounting.py
@@ -4,7 +4,8 @@ import cv2
 import imutils
 import sys
 import tracking
-
+from window import fileName, videoButtonClicked, openTrackingButtonClicked
+from imageProcess import imagePros(fileName)
 
 def checkIfSplit(cells, cx, cy):
 	if checked == False:
@@ -28,52 +29,14 @@ def skipFrame(counter, countarry, trackbarValue):
 countarry= []
 #global counter
 #counter = 0
-video = cv2.VideoCapture(sys.argv[1])
-length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
 cnts = []
 
 
 
 
-def onChange(trackbarValue):
-	video.set(cv2.CAP_PROP_POS_FRAMES,trackbarValue)
-	#print(vidframe)
-	global image
-	err, image = video.read()
-	global image_gray1
-	image_gray1= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-	global image_contours
-	image_contours = image.copy();
-	#Adaptive Histogram Equalization
-	clahe = cv2.createCLAHE(clipLimit=6, tileGridSize=(8,8))
-	global cl1
-	cl1 = clahe.apply(image_gray1)
-	#Gray Scale GaussianBlur
-	global image_gray
-	image_gray = cv2.GaussianBlur(cl1, (3,3), 0,0);
-
-	#image normalize
-	dst = np.zeros(shape=(5,2))
-	global image_norm
-	image_norm= cv2.normalize(image_gray, dst, 0, 255, cv2.NORM_MINMAX)
-
-	#Kernel Definitions
-	kernel = np.ones((3,3),np.uint8)
-	kernel2 = np.ones((1,2), np.uint8)
-
-	#Threshold and Noise Reduction with dilation and erosion
-	global image_edged
-	ret, image_edged = cv2.threshold(image_norm, 115, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU);
-	global image_edged2
-	image_edged2 = cv2.dilate (image_edged, kernel, iterations = 2);
-	global image_edged3
-	image_edged3 = cv2.erode (image_edged2, kernel2, iterations = 3);
-
-	#Find Contours
-	cnts = cv2.findContours(image_edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
-	#print(cnts)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
+def onChange(trackbarValue, fileName):
+	imagePros():
 
 	#Ignore contours that do not satisfy size criteria
 	cells = []
